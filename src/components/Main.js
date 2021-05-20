20769df78892206b0944d61b2ce623c7ff116a67
lookburn <36327225+lookburn@users.noreply.github.com>
2021-05-20 16:51:47 +0100

    forgot to add Main.js file

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class Main extends Component {
+
+    <h1>Hello World</h1>
+
+  render() { // inside curly braces is JS
+    return (
+      <div id="content">
+        <h1></h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
