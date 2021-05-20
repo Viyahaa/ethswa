05d4fc34fad7938df4aedb968e6b46e0054ac017
lookburn <36327225+lookburn@users.noreply.github.com>
2021-05-20 13:15:04 +0100

    sell tokens function complete and tested

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -13,7 +13,15 @@ contract EthSwap {
     uint public rate = 100; // pre-determined exchange rate
 
     // to keep transparency
-    event TokenPurchased(
+    event TokensPurchased(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+
+    // to keep transparency
+    event TokensSold(
         address account,
         address token,
         uint amount,
@@ -40,6 +48,27 @@ contract EthSwap {
         token.transfer(msg.sender, tokenAmount);
 
         //emit event
-        emit TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+        emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    // reverse of buying
+    function sellTokens(uint _amount) public {
+        // user can't sell more tokens than have: ERC-20 handles this as standard
+        // only for tutorial purposes
+        require(token.balanceOf(msg.sender) >= _amount);
+
+        // Calculate amount of ETH to redeem
+        uint etherAmount = _amount / rate;
+
+        // require sufficient ETH at Exchange
+        require(address(this).balance >= etherAmount);
+
+        // Perform sale
+        // transfer function native to ETH
+        // can't call (transfer() on ERC-20 token on behalf of investor)
+        token.transferFrom(msg.sender, address(this), _amount); // trasnfer
+        msg.sender.transfer(etherAmount);
+
+        emit TokensSold(msg.sender, address(token), _amount, rate);
     }
 }
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -94,4 +94,36 @@ contract('EthSwap', ([deployer, investor]) => { //callback function
         })
     })
 
+    describe('sellTokens()', async () => {
+        let result
+
+        before(async () => {
+            // investor must approve tokens before purchase
+            await token.approve(ethSwap.address, tokens('100'), {from: investor})
+            result = await ethSwap.sellTokens(tokens('100'), {from: investor})
+        })
+
+        it('Allows user to instantly sell tokens to EthSwap for a fixed price', async () => {
+            // check investor token balance after sale
+            let investorBalance = await token.balanceOf(investor)
+            assert.equal(investorBalance.toString(), tokens('0'))
+
+            // check EthSwap balance
+            let ethSwapBalance
+            ethSwapBalance = await token.balanceOf(ethSwap.address)
+            assert.equal(ethSwapBalance.toString(), tokens('1000000'))
+            ethSwapBalance = await web3.eth.getBalance(ethSwap.address)
+            assert.equal(ethSwapBalance.toString(), web3.utils.toWei('0', 'Ether'))
+
+            // ensures emit is correct
+            const event = result.logs[0].args
+            assert.equal(event.account, investor)
+            assert.equal(event.token, token.address)
+            assert.equal(event.amount.toString(), tokens('100').toString())
+            assert.equal(event.rate.toString(), '100')
+
+            //FAILURE: investor can't sell more tokens than available
+            await ethSwap.sellTokens(tokens('500'), {from: investor}).should.be.rejected;
+        })
+    })
 })
