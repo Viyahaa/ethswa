c92f91c9d90dd7fa45c6abe0fea7eb31f0b40972
lookburn <36327225+lookburn@users.noreply.github.com>
2021-05-20 16:28:12 +0100

    front-end: added balance and generated identicon

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3'
+import NavBar from './NavBar'
 import './App.css';
 
 class App extends Component {
-  render() {
+
+    async componentWillMount() {
+        await this.loadWeb3()
+        await this.loadBlockchainData()
+    }
+
+    async loadBlockchainData() {
+        const web3 = window.web3
+
+        // get accounts to display balance (e.g. max to buy)
+        const accounts = await web3.eth.getAccounts()
+
+        // set state to access the data in other places in the application
+        this.setState({ account: accounts[0]})
+
+        const ethBalance = await web3.eth.getBalance(this.state.account)
+        this.setState({ ethBalance }) // same as ethBalance: ethBalance
+    }
+
+    async loadWeb3() {
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+        }
+        else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+        }
+        else {
+            window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+        }
+    }
+
+    constructor(props) {
+    super(props) // calls constructor on class we are extending
+    this.state = {
+        account: ''
+    }
+  }
+
+  render() { // inside curly braces is JS
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <NavBar account={this.state.account}/> // Puts NavBar in its own component & sends state to access balance
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
@@ -25,20 +56,8 @@ class App extends Component {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={logo} className="App-logo" alt="logo" />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                <h1>Hello World</h1>
               </div>
             </main>
           </div>
